34cae3c1904bf9170be82304e5d57ef3661a2c4d
Kent <kentnathanael06@gmail.com>
2025-02-25 22:50:14 +0700

    Completed Odd Exercise

--- LatihanUTP/Quiz1odd20.cpp
+++ LatihanUTP/Quiz1odd20.cpp
@@ -1,71 +1,291 @@
 // NO 3
 
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+
+// struct tnode{
+//     char nama[100];
+//     int fuel;
+//     int emergency;
+//     tnode* next;
+// }*h;
+
+// void display(){
+//     tnode* temp = h;
+//     while(temp != NULL){
+//         printf("%s -> ", temp->nama);
+//         temp = temp->next;
+//     }
+//     printf("NULL\n");
+// }
+
+// void enqueue(char *nama, int fuel, int emer){
+//     tnode* c = (tnode*)malloc(sizeof(tnode));
+//     strcpy(c->nama, nama);
+//     c->fuel = fuel;
+//     c->emergency = emer;
+//     c->next = NULL;
+
+//     if(!h){
+//         h = c;
+//     }
+//     else if( (c->emergency > h->emergency) || (c->emergency == h->emergency && c->fuel < h->fuel)){
+//         c->next = h;
+//         h = c;
+//     }
+//     else{
+//         tnode* temp = h;
+//         while(temp->next != NULL && ((temp->next->emergency == c->emergency && temp->next->fuel <= c->fuel) 
+//         || temp->next->emergency > c->emergency)){
+//             temp = temp->next;
+//         }
+//         c->next = temp->next;
+//         temp->next = c;
+//     }
+
+// }
+
+// void dequeue(){
+//     display();
+
+//     tnode* temp = h;
+//     h = h->next;
+//     free(temp);
+
+//     display();
+// }
+
+
+// int main(){
+//     enqueue("Apollo", 75, 0);
+//     enqueue("Voyager", 50, 0);
+//     enqueue("Odyssey", 90, 1);
+//     enqueue("Endeavour", 15, 0);
+//     enqueue("Pioneer", 80, 1);
+
+//     dequeue();
+// }
+
+// no 4
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#define hash_size 100
+#define gc getchar();
 
 struct tnode{
-    char nama[100];
-    int fuel;
-    int emergency;
+    char plate[100];
+    char name[100];
+    char ID[5];
     tnode* next;
-}*h;
+}*arr[hash_size];
 
-void display(){
-    tnode* temp = h;
-    while(temp != NULL){
-        printf("%s -> ", temp->nama);
-        temp = temp->next;
-    }
-    printf("NULL\n");
+void printMenu(){
+    puts("Parking System");
+    puts("===============");
+    puts("1. ADD");
+    puts("2. VERIFY");
+    puts("3. VIEW");
+    puts("4. REMOVE");
+    puts("5. EXIT");
+    printf("Input : ");
 }
 
-void enqueue(char *nama, int fuel, int emer){
-    tnode* c = (tnode*)malloc(sizeof(tnode));
-    strcpy(c->nama, nama);
-    c->fuel = fuel;
-    c->emergency = emer;
-    c->next = NULL;
+int hash(char *plate){
+    int key = 0;
 
-    if(!h){
-        h = c;
+    for(int i = 0; i < strlen(plate); i++){
+        key += plate[i];
     }
-    else if( (c->emergency > h->emergency) || (c->emergency == h->emergency && c->fuel < h->fuel)){
-        c->next = h;
-        h = c;
+
+    key *= key;
+    
+    char temp[100];
+    sprintf(temp, "%d", key);
+
+    if(strlen(temp)%2==0){
+        char loc[3] = {plate[strlen(plate)/2-1], plate[strlen(plate)/2], '\0'};
+
+        key = atoi(loc);
+
+        return key % hash_size;
     }
     else{
-        tnode* temp = h;
-        while(temp->next != NULL && ((temp->next->emergency == c->emergency && temp->next->fuel <= c->fuel) 
-        || temp->next->emergency > c->emergency)){
+        char loc[3] = {plate[strlen(plate)/2-1], '\0'};
+        
+        key = atoi(loc);
+
+        return key % hash_size;
+    }
+}
+
+void push(char *plate, char *name, char *Id){
+    tnode* c = (tnode*)malloc(sizeof(tnode));
+    int index = hash(plate);
+    printf("%d ", index);
+    strcpy(c->name, name);
+    strcpy(c->plate, plate);
+    strcpy(c->ID, Id);
+    c->next = NULL;
+
+    if(arr[index] == NULL){
+        arr[index] = c;
+    }
+    else{
+        tnode* temp = arr[index];
+        while(temp->next){
             temp = temp->next;
         }
         c->next = temp->next;
         temp->next = c;
     }
-
-    // display();
 }
 
-void dequeue(){
-    display();
+void add(){
+    char plate[100];
+    char name[100];
+    char ID[6];
 
-    tnode* temp = h;
-    h = h->next;
-    free(temp);
+    printf("Enter License plate : ");
+    scanf("%s", plate); gc
 
-    display();
+    printf("Enter Owner Name : ");
+    scanf("%s", name); gc;
+
+    for(int i = 0; i <= 5; i++){
+        if(i == 5){
+            ID[i] = '\0';
+            break;
+        }
+        int choice = rand()%2 + 0;
+
+        if(choice == 0){
+            char capital = rand()%26 + 'A'; 
+            ID[i] =  capital;
+        }
+        else if(choice == 1){
+            char lower =  rand()%26 + 'a';
+            ID[i] = lower;
+        }
+        else{
+            char number = rand()%10 + '0';
+            ID[i] = number;
+        }
+    }
+
+    push(plate, name, ID);
+    printf("Parking succesfully!\n");
+    printf("Your Parking code : %s\n", ID);
 }
 
+void verify(){
+    char ID[6] = {};
+
+    printf("Enter Parking code : ");
+    scanf("%s", ID); gc;
+
+    for(int i = 0; i < hash_size; i++){
+        if(arr[i] == NULL){
+            continue;
+        }
+        else{
+            tnode* temp = arr[i];
+            int flag = 0;
+            while(temp){
+                if(strcmp(ID, temp->ID)==0){
+                    printf("Vehicle : %s\nOwner : %s\n", temp->plate, temp->name);
+                    flag = 1;
+                    break;
+                }
+                temp = temp->next;
+            }
+            if(flag == 0) printf("Error");
+        }
+    }
+}
+
+void view(){
+    for(int i = 0; i < hash_size; i++){
+        if(arr[i] == NULL){
+            continue;
+        }
+        else{
+            tnode* temp = arr[i];
+            while(temp){
+                printf("%s %s %s\n", temp->plate, temp->name, temp->ID);
+                temp = temp->next;
+            }
+        }
+    }
+}
+
+void remove(){
+    char ID[5];
+
+    printf("Enter Parking code : ");
+    scanf("%s", ID); gc;
+
+    for(int i = 0; i < hash_size; i++){
+        if(arr[i] == NULL){
+            continue;
+        }
+        else{
+            tnode* temp = arr[i];
+            int flag = 0;
+            if(strcmp(temp->ID, ID)==0){
+                printf("Vehicle %s has exited\n", temp->plate);
+                arr[i] = temp->next;
+                free(temp);
+                flag = 1;
+                break;
+            }
+            else{
+                tnode* temp2 = temp->next;
+                while(temp2 != NULL && strcmp(temp2->ID, ID)!= 0){
+                    temp = temp2;
+                    temp2 = temp2->next;
+                }
+                printf("Vehicle %s has exited\n", temp2->plate);
+                temp->next = temp2->next;
+                free(temp2);
+                flag = 1;
+                break;
+            }
+            if(flag == 0) printf("Error");
+        }
+    }
+}
 
 int main(){
-    enqueue("Apollo", 75, 0);
-    enqueue("Voyager", 50, 0);
-    enqueue("Odyssey", 90, 1);
-    enqueue("Endeavour", 15, 0);
-    enqueue("Pioneer", 80, 1);
+    srand(time(NULL));
+    int choice = -1;
+    do{
+        do{
+            printMenu();
+            scanf("%d", &choice); gc
+        }while(choice < 0 && choice > 5);
 
-    dequeue();
+        switch (choice){
+            case 1: 
+                add();
+                break;
+            case 2:
+                verify();
+                break;
+            case 3:
+                view();
+                break;
+            case 4:
+                remove();
+                break;
+            case 5: 
+                printf("Exiting Program\n");
+                return 0 ;
+        }
+    }while(1);
 
-    // display();
+    return 0;
 }
